fea6add99dc5d26f8116217a3c79418a3b842ce0
Aushraful Alam <aushraful@gmail.com>
2022-12-31 00:44:39 +0600

    Model's prepared

--- app/Models/ProductVariantPrice.php
+++ app/Models/ProductVariantPrice.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductVariantPrice extends Model
 {
+    protected $guarded = [];
 
+    public function p_v_one()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_one');
+    }
+    public function p_v_two()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_two');
+    }
+    public function p_v_three()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_three');
+    }
 }
